3f25b340537452a821b9c77786e1cb86bbd3a5ae
jooheon <cherish2285@naver.com>
2021-07-22 14:41:58 +0900

    algo

--- Main.java
+++ Main.java
@@ -1,57 +1,57 @@
-package light_switch;
+package pizza_boxes;
 
 import java.util.Scanner;
 
 public class Main {
-	static int N = 3;
-	static int[] to_light;
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = Integer.parseInt(sc.nextLine());
-		int[] from_light = new int[N];
-		int[] copy_light = new int[N];
-		to_light = new int[N];
-		String str = sc.nextLine();
-		for (int i = 0; i < N; i++) {
-			from_light[i] = str.charAt(i) - '0';
-			copy_light[i] = str.charAt(i) - '0'; // 첫 전구 키는 상황
-		} // end of input A lights
-		str = sc.nextLine();
-		for (int i = 0; i < N; i++) {
-			to_light[i] = str.charAt(i) - '0';
-		} // end of input B lights
+		long result = 0;
+		int row = sc.nextInt();
+		int col = sc.nextInt();
 
-		int count = calc(from_light);
-		copy_light[0] = copy_light[0] == 0 ? 1 : 0;
-		copy_light[1] = copy_light[1] == 0 ? 1 : 0;
-		int copy_count = calc(copy_light); // count + 1
+		int[][] pizzas = new int[row][col];
+		int[][] copy_pizzas = new int[row][col];
 
-		if (count == -1 && copy_count == -1)
-			System.out.println(-1);
-		else if (count == -1 || copy_count == -1)
-			System.out.println(Math.max(count, copy_count + 1));
-		else
-			System.out.println(Math.min(count, copy_count + 1));
-		sc.close();
-	}
-
-	static int calc(int[] lights) {
-		int count = 0;
-		for (int i = 1; i < N; i++) {
-			if (lights[i - 1] != to_light[i - 1]) {
-				reverse(i, lights);
-				count++;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				pizzas[i][j] = sc.nextInt();
+				copy_pizzas[i][j] = pizzas[i][j];
+				result += pizzas[i][j];
 			}
 		}
-		return lights[N - 1] == to_light[N - 1] ? count : -1;
-	}
 
-	static void reverse(int idx, int[] lights) {
-		lights[idx] = lights[idx] == 0 ? 1 : 0;
-		lights[idx - 1] = lights[idx - 1] == 0 ? 1 : 0;
-		if (idx + 1 < N)
-			lights[idx + 1] = lights[idx + 1] == 0 ? 1 : 0;
-	}
+		// front view
+		for (int i = 0; i < row; i++) {
+			int max_height = 0;
+			int x = 0;
+			int y = 0;
+			for (int j = 0; j < col; j++) {
+				if (max_height < pizzas[i][j]) {
+					max_height = pizzas[i][j];
+					x = j;
+					y = i;
+				}
+			}
+			result -= copy_pizzas[y][x];
+			copy_pizzas[y][x] = 0;
+		}
 
+		// side view
+		for (int i = 0; i < col; i++) {
+			int max_height = 0;
+			int x = 0;
+			int y = 0;
+			for (int j = 0; j < row; j++) {
+				if (max_height < pizzas[j][i]) {
+					max_height = pizzas[j][i];
+					x = i;
+					y = j;
+				}
+			}
+			result -= copy_pizzas[y][x];
+			copy_pizzas[y][x] = 0;
+		}
+
+		System.out.println(result);
+	}
 }
