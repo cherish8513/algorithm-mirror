93a5d8bc8c420f51b04f86409c2217af0c5a8310
jooheon <cherish2285@naver.com>
2021-07-23 15:16:58 +0900

    stack

--- Main.java
+++ Main.java
@@ -1,33 +1,50 @@
-package parenthesis;
+package stack_example;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	static int T;
-	static String pt;
+	static int N;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		T = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < T; i++) {
-			pt = sc.nextLine();
-			int count = 0;
-			for (int j = 0; j < pt.length(); j++) {
-				if (pt.charAt(j) == '(')
-					count++;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		N = Integer.parseInt(br.readLine());
+		int[] stack = new int[N];
+		int count = 0;
+
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			if (str.contains("push")) {
+				String[] push = str.split(" ");
+				count++;
+				stack[count] = Integer.parseInt(push[1]);
+			} else if (str.contains("top")) {
+				if (stack[count] == 0)
+					bw.write("-1\n");
 				else
+					bw.write(stack[count] + "\n");
+			} else if (str.contains("pop")) {
+				if (stack[count] == 0)
+					bw.write("-1\n");
+				else {
+					bw.write(stack[count] + "\n");
 					count--;
-
-				if (count < 0) {
-					break;
 				}
+			} else if (str.contains("empty")) {
+				bw.write((count == 0 ? 1 : 0) + "\n");
 			}
-			if (count == 0)
-				System.out.println("YES");
-			else
-				System.out.println("NO");
-		}
-		sc.close();
-	}
 
+			else if (str.contains("size")) {
+				bw.write(count + "\n");
+			}
+		}
+		bw.flush();
+		br.close();
+		bw.close();
+	}
 }
