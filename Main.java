17ee41079fcfddf57ea3522d6aabaffff8beef49
jooheon <cherish2285@naver.com>
2021-07-27 05:58:28 +0900

    stick

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package balanced_world;
+package iron_stick;
 
 import java.util.Scanner;
 
@@ -6,46 +6,22 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
-		char c;
-		while (!(str.equals("."))) {
-			int check = 0;
-			int count_so = 0;
-			int count_dae = 0;
-
-			char[] arr = new char[100];
-			int idx = 0;
-			while ((c = str.charAt(check)) != '.') {
-				check++;
-				if (c == '(') {
-					count_so++;
-					idx++;
-					arr[idx] = c;
-				} else if (c == ')') {
-					count_so--;
-					if (arr[idx] != '(')
-						break;
-					else
-						idx--;
-				} else if (c == '[') {
-					count_dae++;
-					idx++;
-					arr[idx] = c;
-				} else if (c == ']') {
-					count_dae--;
-					if (arr[idx] != '[')
-						break;
-					else
-						idx--;
-				}
-				if (count_so < 0 || count_dae < 0)
-					break;
+		int count = 0;
+		int result = 0;
+		
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '(') count++;
+			else {
+				count--;
+				
+				if(str.charAt(i-1) == '(')
+					result += count;
+				else
+					result++;
 			}
-			if (count_so == 0 && count_dae == 0)
-				System.out.println("yes");
-			else
-				System.out.println("no");
-			str = sc.nextLine();
 		}
+		
+		System.out.println(result);
 		sc.close();
 	}
 }
