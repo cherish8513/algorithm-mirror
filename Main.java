ec05d041ad48086a47f626b7d51a225922c71a41
jooheon <cherish2285@naver.com>
2021-07-27 10:37:56 +0900

    card

--- Main.java
+++ Main.java
@@ -1,45 +1,25 @@
-package nge;
+package card_two;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-	static int value = 1;
-	static int valueOfIdx = 0;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		Stack<Integer[]> stack = new Stack<>();
-		int[] arr = new int[n];
-		int idx = 0;
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			int next = Integer.parseInt(st.nextToken());
-			while (!stack.isEmpty() && stack.peek()[value] < next) {
-				arr[stack.peek()[valueOfIdx]] = next;
-				stack.pop();
-			}
-			Integer[] val = {idx, next};
-			stack.add(val);
-			idx++;
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>();
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		
+		for(int i = 1; i <= n; i++) {
+			q.add(i);
 		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < n - 1; i++) {
-			if (arr[i] == 0)
-				sb.append(-1).append(' ');
-			else
-				sb.append(arr[i]).append(' ');
+		
+		while(q.size() != 1) {
+			q.poll();
+			int second = q.poll();
+			q.add(second);
 		}
-		sb.append(-1);
-		bw.write(sb+"\n");
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(q.poll());
+		sc.close();
 	}
 }
