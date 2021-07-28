8e42252556626c0dbe8f6a5fe83b865f4091cfea
jooheon <cherish2285@naver.com>
2021-07-28 20:40:43 +0900

    yosepus 07_28

--- Main.java
+++ Main.java
@@ -1,55 +1,37 @@
-package queue_two;
+package yosepus;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+/**
+ * 
+ * @author jooheon
+ * Since 2021.07.28
+ * 요세푸스 수열 출력
+ *
+ */
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		int[] queue = new int[2000000];
-		int front = 0;
-		int rear = 1;
-		for (int i = 0; i < n; i++) {
-			String order = br.readLine();
-			if (order.contains("push")) {
-				front++;
-				String[] push = order.split(" ");
-				queue[front] = Integer.parseInt(push[1]);
-			} else if (order.contains("pop")) {
-				if (front < rear)
-					bw.write(-1 + "\n");
-				else {
-					bw.write(queue[rear] + "\n");
-					rear++;
-				}
-			} else if (order.contains("size")) {
-				bw.write(front-rear+1 + "\n");
-			} else if (order.contains("empty")) {
-				if (front < rear)
-					bw.write(1 + "\n");
-				else
-					bw.write(0 + "\n");
-			} else if (order.contains("front")) {
-				if (front < rear)
-					bw.write(-1 + "\n");
-				else {
-					bw.write(queue[rear] + "\n");
-				}
-			} else if (order.contains("back")) {
-				if (front < rear)
-					bw.write(-1 + "\n");
-				else {
-					bw.write(queue[front] + "\n");
-				}
-			}
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>(); // PriorityQueue로 받으면 Stack과 거의 같다.
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		for(int i = 1 ; i <= N; i++) { // Queue에 1부터 N까지 넣는다.
+			q.add(i);
 		}
-		bw.flush();
-		bw.close();
-		br.close();
+		StringBuilder sb = new StringBuilder(); // 출력 결과용 StringBuilder 준비
+		sb.append("<"); // 첫 문자인 < 저장
+		while(!q.isEmpty()) {
+			for(int i = 0; i < K-1; i++) { // K-1 횟수만큼 빼고 맨 뒤로 넣는다.
+				int num = q.poll();
+				q.add(num);
+			}
+			sb.append(q.poll() + ", "); // K번째를 빼고 StringBuilder에 저장
+		}
+		sb.deleteCharAt(sb.lastIndexOf(",")); // 마지막 ,를 지운다.
+		sb.deleteCharAt(sb.lastIndexOf(" ")); // 마지막 공백을 지운다.
+		sb.append(">"); // 마지막 문자인 > 저장
+		System.out.println(sb); // 출력
+		sc.close();
 	}
 }
