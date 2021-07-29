7a79a94dcf53c4a3ed6708bb7a7cd67639f1e7f3
jooheon <cherish2285@naver.com>
2021-07-29 11:30:18 +0900

    what is recursive

--- Main.java
+++ Main.java
@@ -1,25 +1,34 @@
-package fibonacci;
+package recursive;
 
 import java.util.Scanner;
 
 public class Main {
-	static int[] dp = new int[100];
+	static int limit;
+	static String str = "____";
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println(fibo(Integer.parseInt(sc.nextLine())));
+		limit = sc.nextInt();
+		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+		recursive(0);
 		sc.close();
 	}
 	
-	static int fibo(int target) {
-		if(target == 0)
-			return 0;
-		if(target <= 2)
-			return 1;
-		if(dp[target] != 0)
-			return dp[target];
-		else {
-			dp[target] = fibo(target-1) + fibo(target-2);
-			return dp[target];
+	static void recursive(int time) {
+		String str_output = "";
+		for(int i = 0; i < time; i++) {
+			str_output += str;
 		}
+		if(time == limit) {
+			System.out.println(str_output + "\"재귀함수가 뭔가요?\"");
+			System.out.println(str_output + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+			System.out.println(str_output + "라고 답변하였지.");
+			return;
+		}
+		System.out.println(str_output + "\"재귀함수가 뭔가요?\"");
+		System.out.println(str_output + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+		System.out.println(str_output + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+		System.out.println(str_output + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+		recursive(time+1);
+		System.out.println(str_output + "라고 답변하였지.");
 	}
 }
