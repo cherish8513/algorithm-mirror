908c154c0dc43d9912137cfde5f0e9216d5d6ca9
jooheon <cherish2285@naver.com>
2021-08-02 13:42:19 +0900

    star

--- Main.java
+++ Main.java
@@ -1,51 +1,42 @@
-package big_number;
+package star;
 
 import java.util.Scanner;
 
 public class Main {
-	static int target;
-	static int[] arr;
-	static int N;
-	static int result = 0;
-	static int target_depth;
+	static String[] part = {"****", "* *", "**", "*"};
+	static char[][] map;
+	static int N; 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String[] str = sc.nextLine().split(" ");
-		target = Integer.parseInt(str[0]);
-		target_depth = str[0].length();
-		int input_max_value = (str[0].charAt(0) - '0');
-		N = Integer.parseInt(str[1]);
-		arr = new int[N];
-		str = sc.nextLine().split(" ");
-		boolean check = true;
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(str[i]);
-			if(arr[i] < (input_max_value))
-				check = false;
+		N = sc.nextInt();
+		int n = 1 << N;
+		map = new char[n][n];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				map[i][j] = ' ';
+			}
 		}
-		if(check)
-			target_depth--;
-		dfs(0, new int[target_depth]);
-		System.out.println(result);
+		star(n, 0, 0);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				sb.append(map[i][j]);
+			}
+			System.out.println(sb.substring(0, sb.lastIndexOf("*")+1));
+			sb = new StringBuilder();
+		}
 		sc.close();
 		
 	}
 	
-	static void dfs(int depth, int[] candidate){
-		if(depth == target_depth) {
-			StringBuilder sb = new StringBuilder();
-			for(int i : candidate)
-				sb.append(i);
-			String str = sb.substring(0);
-			int value = Integer.parseInt(str);
-			if(value <= target)
-				result = Math.max(result, value);
+	static void star(int n, int y, int x) {
+		if(n == 1) {
+			map[y][x] = '*';
 			return;
 		}
-		for(int i = 0; i < N; i++) {
-			candidate[depth] = arr[i];
-			dfs(depth+1, candidate);
-		}
-			
+		n /= 2;
+		star(n, y, x);
+		star(n, y+n, x);
+		star(n, y, x+n);
 	}
 }
