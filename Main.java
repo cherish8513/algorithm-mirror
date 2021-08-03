cd583896a38150601657af061abb316adfb4af32
jooheon <cherish2285@naver.com>
2021-08-03 10:45:02 +0900

    virus

--- Main.java
+++ Main.java
@@ -1,42 +1,44 @@
-package star;
+package virus;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	static String[] part = {"****", "* *", "**", "*"};
-	static char[][] map;
-	static int N; 
+	static ArrayList<ArrayList<Integer>> map = new ArrayList<>();
+	static boolean[] visited;
+	static int result = 0;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		int n = 1 << N;
-		map = new char[n][n];
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				map[i][j] = ' ';
+		int numberOfCom = sc.nextInt();
+		int numbefOfEdge = sc.nextInt();
+		map.add(null); // 0번 인덱스
+		for(int i = 0; i < numberOfCom; i++) {
+			map.add(new ArrayList<Integer>());
+		} // ArrayList 초기화
+		for(int i = 0; i < numbefOfEdge; i++) {
+			int from = sc.nextInt();
+			int to = sc.nextInt();
+			map.get(from).add(to);
+			map.get(to).add(from); // 양방향
+		} // end of input
+		visited = new boolean[numberOfCom + 1];
+		for(int i = 0; i < visited.length; i++) {
+			visited[i] = false;
+		} // 방문정보 초기화
+		dfs(1);
+		System.out.println(result);
+	}
+
+	public static void dfs(int startPoint) {
+		// TODO Auto-generated method stub
+		if(!visited[startPoint]) {
+			visited[startPoint] = true;
+			result++;
+			for(int nextPoint : map.get(startPoint)) {
+				dfs(nextPoint);
 			}
 		}
-		star(n, 0, 0);
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				sb.append(map[i][j]);
-			}
-			System.out.println(sb.substring(0, sb.lastIndexOf("*")+1));
-			sb = new StringBuilder();
-		}
-		sc.close();
-		
 	}
 	
-	static void star(int n, int y, int x) {
-		if(n == 1) {
-			map[y][x] = '*';
-			return;
-		}
-		n /= 2;
-		star(n, y, x);
-		star(n, y+n, x);
-		star(n, y, x+n);
-	}
 }
