6594df04118a3bfd0294d3b8bda76c0b18ae34bf
jooheon <cherish2285@naver.com>
2021-08-04 13:31:27 +0900

    lie lie lie

--- Main.java
+++ Main.java
@@ -1,67 +1,63 @@
-package danji;
+package lie;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static boolean[][] visited;
-	static int[][] map;
-	static ArrayList<Integer> result = new ArrayList<>();
-	static int count;
-	static int N;
+	static List<ArrayList<Integer>> map = new ArrayList<>();
+	static int numberOfPerson;
+	static int numberOfParty;
+	static boolean[] visited;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		map = new int[N+2][N+2];
-		visited = new boolean[N+2][N+2];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		numberOfPerson = Integer.parseInt(st.nextToken());
+		numberOfParty = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine(), " ");
+		int[] numberOfStartPos = new int[Integer.parseInt(st.nextToken())];
+		for (int i = 0; i < numberOfStartPos.length; i++)
+			numberOfStartPos[i] = Integer.parseInt(st.nextToken()); // dfs 위치들 입력받음
+		map.add(null); // 0번 인덱스
+		for (int i = 0; i < numberOfPerson + numberOfParty; i++) {
+			map.add(new ArrayList<Integer>());
+		} // map 초기화
 
-		for (int i = 1; i < N+1; i++) {
-			String str = br.readLine();
-			for (int j = 1; j < N+1; j++) {
-				map[i][j] = Integer.parseInt(str.charAt(j-1) + "");
-				visited[i][j] = false;
+		for (int i = 1; i <= numberOfParty; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			st.nextToken();
+			while (st.hasMoreTokens()) {
+				int person = Integer.parseInt(st.nextToken());
+				map.get(person).add(i + numberOfPerson);
+				map.get(i + numberOfPerson).add(person);
 			}
 		} // end of input
-		
-		for(int i = 0; i < N+2; i++) {
-			for(int j = 0; j < N+2; j++) {
-				System.out.print(map[i][j] + " ");
-			}
-			System.out.println();
+		visited = new boolean[numberOfPerson + numberOfParty + 1];
+		for (int i = 0; i < numberOfParty + numberOfPerson + 1; i++) {
+			visited[i] = false;
 		}
-
-		for (int i = 1; i < N+1; i++) {
-			for (int j = 1; j < N+1; j++) {
-				if (map[i][j] == 1 && !visited[i][j]) {
-					count = 0;
-					dfs(i, j);
-					result.add(count);
-				}
-			}
-		} // end of calculate
-		
-		Collections.sort(result);
-		System.out.println(result.size());
-		for(int i : result) {
-			System.out.println(i);
+		for (int i = 0; i < numberOfStartPos.length; i++) {
+			dfs(numberOfStartPos[i]);
 		}
-	} // end of main
-
-	public static void dfs(int y, int x) {
-		// TODO Auto-generated method stub
-		if (map[y][x] == 0 || visited[y][x])
-			return;
-		count++;
-		visited[y][x] = true;
-		dfs(y+1, x);
-		dfs(y-1, x);
-		dfs(y, x-1);
-		dfs(y, x+1);
-
+		int result = 0;
+		for (int i = numberOfPerson + 1; i < numberOfParty + numberOfPerson + 1; i++) {
+			if(visited[i] == false)
+				result++;
+		}
+		System.out.println(result);
 	}
-} // end of class
+
+	public static void dfs(int startPoint) {
+		// TODO Auto-generated method stub
+		if (!visited[startPoint]) {
+			visited[startPoint] = true;
+			for (int nextPoint : map.get(startPoint)) {
+				dfs(nextPoint);
+			}
+		}
+	}
+}
