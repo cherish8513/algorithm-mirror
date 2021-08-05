2f58e5c1990c5389960451d54f7cc6ab54899d30
jooheon <cherish2285@naver.com>
2021-08-05 11:34:33 +0900

    섬구하기

--- Main.java
+++ Main.java
@@ -1,77 +1,58 @@
-package lie;
+package iland;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
-/**
- * 
- * @author jooheon
- * 사람과 파티를 같은 노드로 보고 진실을 아는 사람 -> 파티 -> 파티에 참석한 사람들 -> 파티 -> ... 방식으로 DFS 탐색
- * 진실된 자들끼리 연결시켜서 1명만 DFS 탐색 하면 되게 변경 (유니온 파인드)
- *
- */
-public class Main {
-	static List<ArrayList<Integer>> map = new ArrayList<>();
-	static int numberOfPerson;
-	static int numberOfParty;
-	static boolean[] visited;
 
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		numberOfPerson = Integer.parseInt(st.nextToken());
-		numberOfParty = Integer.parseInt(st.nextToken()); // 사람의 수 N과 파티의 수 M
-		st = new StringTokenizer(br.readLine(), " ");
-		int[] numberOfStartPos = new int[Integer.parseInt(st.nextToken())];
-		for (int i = 0; i < numberOfStartPos.length; i++)
-			numberOfStartPos[i] = Integer.parseInt(st.nextToken()); // 이야기의 진실을 아는 사람의 수와 번호
-		map.add(null); // 0번 인덱스
-		for (int i = 0; i < numberOfPerson + numberOfParty; i++) {
-			map.add(new ArrayList<Integer>());
-		} // map 초기화
-
-		for (int party_idx = 1; party_idx <= numberOfParty; party_idx++) {
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		while (true) {
 			st = new StringTokenizer(br.readLine(), " ");
-			st.nextToken();
-			while (st.hasMoreTokens()) { // 1차원 배열에 사람들 순서대로 들어가고 그 다음에 파티가 들어감
-				int person = Integer.parseInt(st.nextToken());
-				map.get(person).add(party_idx + numberOfPerson); // 사람에서 파티로
-				map.get(party_idx + numberOfPerson).add(person); // 파티에서 사람으로 연결
+			int w = Integer.parseInt(st.nextToken());
+			int h = Integer.parseInt(st.nextToken());
+			if (w == 0 && h == 0)
+				break;
+			int[][] map = new int[h + 2][w + 2];
+			boolean[][] visited = new boolean[h + 2][w + 2];
+			for (int i = 1; i < h + 1; i++) {
+				String str = br.readLine();
+				for (int j = 1, idx = 0; j < w + 1; j++, idx+=2) {
+					map[i][j] = str.charAt(idx)-'0';
+					visited[i][j] = false;
+				}
+			} // end of for input
+			int output = 0;
+			for (int i = 1; i < h + 1; i++) {
+				for (int j = 1; j < w + 1; j++) {
+					if(map[i][j] == 1 && !visited[i][j]) {
+						dfs(map, visited, i, j);
+						output++;
+					}
+				}
 			}
-		} // end of input
-		visited = new boolean[numberOfPerson + numberOfParty + 1]; //사람과 파티의 방문정보 입력 (1번 인덱스부터라서 + 1 해줌)
-		for (int i = 0; i < numberOfParty + numberOfPerson + 1; i++) {
-			visited[i] = false;
-		}
-		
-		if(numberOfStartPos.length > 0) { // 진실을 아는 자 1명에 나머지 진실을 아는 자 연결
-			for (int i = 1; i < numberOfStartPos.length; i++) {
-				map.get(numberOfStartPos[0]).add(numberOfStartPos[i]);
-			}
-			dfs(numberOfStartPos[0]);
-		}
-//		for (int i = 0; i < numberOfStartPos.length; i++) {
-//			dfs(numberOfStartPos[i]);
-//		}
-		
-		int result = 0;
-		for (int i = numberOfPerson + 1; i < numberOfParty + numberOfPerson + 1; i++) { // 진실을 아는 자가 없는 파티의 수 출력
-			if(visited[i] == false)
-				result++;
-		}
-		System.out.println(result);
-	}
+			sb.append(output).append("\n");
+		} // end of while testCases
+		System.out.println(sb);
+	} // end of main
 
-	public static void dfs(int startPoint) {
+	private static void dfs(int[][] map, boolean[][] visited, int y, int x) {
 		// TODO Auto-generated method stub
-		if (!visited[startPoint]) {
-			visited[startPoint] = true;
-			for (int nextPoint : map.get(startPoint)) {
-				dfs(nextPoint);
-			}
+		if(visited[y][x] || map[y][x] == 0)
+			return;
+		else {
+		visited[y][x] = true;
+		dfs(map, visited, y + 1, x);
+		dfs(map, visited, y + 1, x + 1);
+		dfs(map, visited, y + 1, x - 1);
+		dfs(map, visited, y, x + 1);
+		dfs(map, visited, y, x - 1);
+		dfs(map, visited, y - 1, x);
+		dfs(map, visited, y - 1, x + 1);
+		dfs(map, visited, y - 1, x - 1);
 		}
 	}
-}
+} // end of class
