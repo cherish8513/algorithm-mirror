b18edcef5a151cc57ee5f496db888cf55b26e1ec
jooheon <cherish2285@naver.com>
2021-08-06 19:11:58 +0900

    0806 미로찾기 success

--- Main.java
+++ Main.java
@@ -1,74 +1,55 @@
-package tree_diameter;
+package miro;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int result = 0;
-	static int longest_node_idx = 0;
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static int y_pos = 0;
+	static int x_pos = 1;
+	static int distance = 2;
+	static int[] dy = {0, 0, 1, -1}; 
+	static int[] dx = {1, -1, 0, 0}; 
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int numberOfPoint = Integer.parseInt(br.readLine());
-		List<ArrayList<Info>> map = new ArrayList<>();
-		map.add(null); // 0번째 버림
-		for (int i = 0; i < numberOfPoint; i++) {
-			map.add(new ArrayList<>());
-		} // map 초기화
-		StringTokenizer st;
-		for (int i = 1; i <= numberOfPoint; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int from = Integer.parseInt(st.nextToken());
-			while (st.hasMoreTokens()) {
-				int to = Integer.parseInt(st.nextToken());
-				if (to != -1) {
-					int distance = Integer.parseInt(st.nextToken());
-					map.get(from).add(new Info(to, distance));
-					map.get(to).add(new Info(from, distance));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		char[][] map = new char[N+2][M+2]; // char 맵 테두리 포함 생성
+		for (int i = 1; i < N+1; i++) {
+			String str = br.readLine();
+			for (int j = 1; j < M+1; j++) {
+				map[i][j] = str.charAt(j-1); // 입력값이 char
+			}
+		} // end of for input
+
+		boolean[][] visited = new boolean[N+2][M+2]; // 방문정보 기본 값이 false
+		Queue<Integer[]> q = new LinkedList<>();
+		q.add(new Integer[] {1, 1, 1}); // y, x, distance
+		visited[1][1] = true; // 처음 좌표 방문 true
+		while(!q.isEmpty()) {
+			Integer[] pos = q.poll();
+			for(int i = 0; i < 4; i++) {
+				int ny = pos[y_pos] + dy[i]; // 전역 변수로 y_pos = 0
+				int nx = pos[x_pos] + dx[i]; // 전역 변수로 x_pos = 1
+				int dist = pos[distance] + 1; // 전역 변수로 distance = 2
+				if(ny == N && nx == M) { // 도착 지점이면
+					System.out.println(dist); // 출력
+					System.exit(0); // 0이 정상 종료 (main이 아니었으면 return)
+				}
+				else if(map[ny][nx] != '1' || visited[ny][nx]) {
+					continue;
+				}
+				else {
+					visited[ny][nx] = true;
+					q.add(new Integer[] {ny, nx, dist});
 				}
 			}
-		} // end of input
-		boolean[] visited = new boolean[numberOfPoint + 1];
-		for (int i = 0; i < numberOfPoint + 1; i++) {
-			visited[i] = false;
+			
 		}
-		dfs(map, 1, 0, Arrays.copyOfRange(visited, 0, numberOfPoint + 1));
-		dfs(map, longest_node_idx, 0, Arrays.copyOfRange(visited, 0, numberOfPoint + 1));
-
-		System.out.println(result);
-		br.close();
-	}
-
-	private static void dfs(List<ArrayList<Info>> map, int startPoint, int cur_dist, boolean[] visited) {
-		// TODO Auto-generated method stub
-		if (visited[startPoint]) {
-			return;
-		} else {
-			visited[startPoint] = true;
-			if(result < cur_dist) {
-				result = cur_dist;
-				longest_node_idx = startPoint;
-			}
-			for (Info linkedNode : map.get(startPoint))
-				dfs(map, linkedNode.to, cur_dist + linkedNode.distance, visited);
-		}
-	}
-}
-
-class Info {
-	public int to;
-	public int distance;
-
-	/**
-	 * @param to
-	 * @param distance
-	 */
-	public Info(int to, int distance) {
-		this.to = to;
-		this.distance = distance;
 	}
 }
