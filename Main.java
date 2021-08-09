913bdf31fcbb149dfb9fde4e5034f35494e96ca8
jooheon <cherish2285@naver.com>
2021-08-09 11:09:01 +0900

    숨바꼭질 success

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package cabbage;
+package sumbaggokjil;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,47 +6,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static boolean[][] visited;
-	private static int[][] map;
+	static int MAX_SIZE = 100000;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
-			map = new int[N + 2][M + 2];
-			for(int i = 0; i < K; i++) {
-				st = new StringTokenizer(br.readLine());
-				int y = Integer.parseInt(st.nextToken());
-				int x = Integer.parseInt(st.nextToken());
-				map[y + 1][x + 1] =  1;
-			} // end of for input
-			visited = new boolean[N+2][M+2];
-			int count = 0;
-			for(int i = 1; i < N+1; i++) {
-				for(int j = 1; j < M+1; j++) {
-					if(map[i][j] != 0 && !visited[i][j]) {
-						dfs(i,j);
-						count++;
-					}
-				}
-			}
-			System.out.println(count);
-		} // end of for testCases
-		
-	} // end of main
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken()); // end of input
 
-	private static void dfs(int y, int x) {
-		// TODO Auto-generated method stub
-		if(visited[y][x] || map[y][x] == 0)
-			return;
-		visited[y][x] = true;
-		dfs(y-1, x);
-		dfs(y+1, x);
-		dfs(y, x-1);
-		dfs(y, x+1);
+		if (N >= K) { // 동생보다 크거나 같은 경우
+			System.out.println(N - K);
+			System.exit(0);
+		}
+		
+		int[] dp = new int[MAX_SIZE + 3]; // 최대 값 + 1 인덱스의 공간 + 1
+		
+		for(int i = N, j = 0; i >= 0; i--, j++) { // N 이하 값들 뒤로 1칸씩
+			dp[i] = j;
+		}
+
+		
+		for (int i = N; i <= K + 1; i++) {
+			dp[i + 1] = dp[i] + 1; // 다음 위치에 일단 + 1 더한 값을 저장
+
+			if ((i + 1) % 2 == 0 && dp[(i + 1) / 2] != -1) // 다음 위치 / 2 부분에 값이 있다면
+				dp[i + 1] = Math.min(dp[(i + 1) / 2] + 1, dp[i + 1]); // / 2 한 부분의 값 + 1과 현재 위치 값 + 1을 비교
+			
+			if (dp[i + 1] < dp[i]) // 다음 위치 값보다 현재 위치 값이 크다면 (뒤로 1칸)
+				dp[i] = dp[i + 1] + 1;
+		}
+		System.out.println(dp[K]);
+		br.close();
 	}
-} // end of class
+}
