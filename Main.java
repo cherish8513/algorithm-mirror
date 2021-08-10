cd65ffbb0581add62202686a3dce7150283620d9
jooheon <cherish2285@naver.com>
2021-08-10 13:54:39 +0900

    2225 합분해 success

--- Main.java
+++ Main.java
@@ -1,41 +1,46 @@
-package sumbaggokjil;
+package plus;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ * 
+ * @author jooheon
+ * 1개를 고르는 경우는 모든 수에 대해서 1
+ * 2개를 고르는 경우는 이전 1개를 고르는 경우의 수 + 이전 2개를 고르는 경우의 수 
+ * n개를 고르는 경우는 이전 n-1개를 고르는 경우의 수 + 이전 n개를 고르는 경우의 수
+ * 근데 왜 20을 2개로 뽑는 경우의 수가 21이지??? 2를 뽑는 경우의 수 1 -> 20은 19가 되야하는디
+ * 아 0도 포함이구나 위의 공식은 1부터 인 경우로 폐기 시간만 날렸네
+ * 1개를 고르는 경우는 모든 수에 대해서 1
+ * 2개를 고르는 경우는 n+1
+ * n개를 고르는 경우의 수는 현재 인덱스 n-1개를 고르는 경우의 수 + 이전 n개를 고르는 경우의 수
+ */
 public class Main {
-	static int MAX_SIZE = 100000;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken()); // end of input
-
-		if (N >= K) { // 동생보다 크거나 같은 경우
-			System.out.println(N - K);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[][] dp = new int[m+1][n+1];
+		Arrays.fill(dp[1], 1);
+		
+		if(m == 1) {
+			System.out.println(1);
 			System.exit(0);
 		}
 		
-		int[] dp = new int[MAX_SIZE + 3]; // 최대 값 + 1 인덱스의 공간 + 1
-		
-		for(int i = N, j = 0; i >= 0; i--, j++) { // N 이하 값들 뒤로 1칸씩
-			dp[i] = j;
+		for(int i = 1; i <= m; i++) { // 0을 만드는 조합은 1개
+			dp[i][0] = 1;
 		}
-
 		
-		for (int i = N; i <= K + 1; i++) {
-			dp[i + 1] = dp[i] + 1; // 다음 위치에 일단 + 1 더한 값을 저장
-
-			if ((i + 1) % 2 == 0 && dp[(i + 1) / 2] != -1) // 다음 위치 / 2 부분에 값이 있다면
-				dp[i + 1] = Math.min(dp[(i + 1) / 2] + 1, dp[i + 1]); // / 2 한 부분의 값 + 1과 현재 위치 값 + 1을 비교
-			
-			if (dp[i + 1] < dp[i]) // 다음 위치 값보다 현재 위치 값이 크다면 (뒤로 1칸)
-				dp[i] = dp[i + 1] + 1;
+		for(int i = 0; i <= n; i++) { // 2개로 뽑는 경우의 수는 n + 1
+			dp[2][i] = i + 1;
 		}
-		System.out.println(dp[K]);
-		br.close();
+		
+		for(int i = 1; i <= m; i++) {
+			for(int j = 1; j <= n; j++) {
+				dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000;
+			}
+		}
+		System.out.println(dp[m][n]);
+		sc.close();
 	}
 }
