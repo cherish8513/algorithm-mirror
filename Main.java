912cbf3eb249e4befedc60e21dcdb0666b64b598
jooheon <cherish2285@naver.com>
2021-08-10 13:59:10 +0900

    1 2 3더하기 success

--- Main.java
+++ Main.java
@@ -1,46 +1,24 @@
-package plus;
+package one_two_three;
 
-import java.util.Arrays;
-import java.util.Scanner;
-/**
- * 
- * @author jooheon
- * 1개를 고르는 경우는 모든 수에 대해서 1
- * 2개를 고르는 경우는 이전 1개를 고르는 경우의 수 + 이전 2개를 고르는 경우의 수 
- * n개를 고르는 경우는 이전 n-1개를 고르는 경우의 수 + 이전 n개를 고르는 경우의 수
- * 근데 왜 20을 2개로 뽑는 경우의 수가 21이지??? 2를 뽑는 경우의 수 1 -> 20은 19가 되야하는디
- * 아 0도 포함이구나 위의 공식은 1부터 인 경우로 폐기 시간만 날렸네
- * 1개를 고르는 경우는 모든 수에 대해서 1
- * 2개를 고르는 경우는 n+1
- * n개를 고르는 경우의 수는 현재 인덱스 n-1개를 고르는 경우의 수 + 이전 n개를 고르는 경우의 수
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int[][] dp = new int[m+1][n+1];
-		Arrays.fill(dp[1], 1);
-		
-		if(m == 1) {
-			System.out.println(1);
-			System.exit(0);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int[] dp = new int[11];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for (int i = 4; i <= 10; i++)
+			dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+		for (int t = 0; t < T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			sb.append(dp[N]).append("\n");
 		}
-		
-		for(int i = 1; i <= m; i++) { // 0을 만드는 조합은 1개
-			dp[i][0] = 1;
-		}
-		
-		for(int i = 0; i <= n; i++) { // 2개로 뽑는 경우의 수는 n + 1
-			dp[2][i] = i + 1;
-		}
-		
-		for(int i = 1; i <= m; i++) {
-			for(int j = 1; j <= n; j++) {
-				dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000;
-			}
-		}
-		System.out.println(dp[m][n]);
-		sc.close();
+		System.out.println(sb);
 	}
 }
