a873137f79d0dda630c9f292d81f7f62261da784
jooheon <cherish2285@naver.com>
2021-08-17 16:29:11 +0900

    1174 success

--- Main.java
+++ Main.java
@@ -1,50 +1,35 @@
-package backjoon_1520_decend;
+package baekjoon_1174_줄어드는_숫자;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	static int[][] dp;
-	static int[][] map;
-	static int N;
-	static int M;
+	private static int[] candidate;
+	private static ArrayList<Long> list;
+
 	public static void main(String[] args) {
+		candidate = new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; // 10개 조합
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		map = new int[N + 2][M + 2];
-		dp = new int[N + 2][M + 2];
-		for (int i = 0; i < N + 2; i++) {
-			for (int j = 0; j < M + 2; j++) {
-				map[i][j] = 10001;
-			}
-		} // 테두리 10001로 초기화
-		for (int i = 1; i < N + 1; i++) {
-			for (int j = 1; j < M + 1; j++) {
-				map[i][j] = sc.nextInt();
-			}
-		} // end of for input
-		for (int i = 0; i < N + 2; i++) {
-			for (int j = 0; j < M + 2; j++) {
-				dp[i][j] = -1;
-			}
+		list = new ArrayList<Long>();
+		int n = sc.nextInt();
+		if(n > 1023) { // 중복 제거하니 들어가는 사이즈가 1024개
+			System.out.println(-1);
+			System.exit(0);
 		}
-		System.out.println(dfs(1, 1, 10001));
-	}// end of main
+		search(0, 0);
+		list.sort(null); // 자릿수 순서로 정렬
+		System.out.println(list.get(n-1));
+}
+	
 
-	private static int dfs(int i, int j, int height) {
+	private static void search(long digit, int idx) {
 		// TODO Auto-generated method stub
-		
-		if(i == N && j == M && height > map[i][j]) {
-			return 1;
+		if(!list.contains(digit)) // 조합인데 중복이 왜 생겼지? 빡치네
+			list.add(digit);
+		if(idx >= 10) {
+			return;
 		}
-		
-		if(map[i][j] >= height)
-			return 0;
-
-		if(dp[i][j] != -1)
-			return dp[i][j];
-		
-		return dp[i][j] = dfs(i - 1, j, map[i][j]) + dfs(i + 1, j, map[i][j]) + 
-				dfs(i, j - 1, map[i][j]) + dfs(i, j + 1, map[i][j]);
+		search((digit * 10) + candidate[idx], idx + 1);
+		search(digit, idx + 1);
 	}
-} // end of class
+}
