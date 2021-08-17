e9b340bead9a5e6ade5fbc6785a0604b1a7bb68a
jooheon <cherish2285@naver.com>
2021-08-17 16:31:08 +0900

    19236_success

--- Main.java
+++ Main.java
@@ -1,34 +1,101 @@
-package baekjoon_9465_스티커;
+package baekjoon_19236_청소년상어;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		for(int t = 0; t < T; t++) {
-			int n = Integer.parseInt(br.readLine());
-			int[][] arr = new int[2][n];
-			for(int i = 0; i< 2; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < n; j++) {
-					arr[i][j] = Integer.parseInt(st.nextToken());
-				}
-			} // end of for input
-			
-			int[][] dp = new int[2][n + 2]; // 알고리즘 일반화를 위해 2칸 추가
-			for(int i = 2; i < n+2; i++) {
-				dp[0][i] = Math.max(dp[1][i-2], dp[1][i-1]) + arr[0][i-2];
-				dp[1][i] = Math.max(dp[0][i-2], dp[0][i-1]) + arr[1][i-2];
+	private static int n;
+	private static int died = -1;
+	private static int shark = 0;
+	private static int[] dx = { 0, -1, -1, -1, 0, 1, 1, 1 };
+	private static int[] dy = { -1, -1, 0, 1, 1, 1, 0, -1 };
+	private static int max_cnt;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = 4;
+		int[][] map = new int[n + 2][n + 2];
+		List<Integer[]> fishs = new ArrayList<Integer[]>();
+		for (int i = 0; i <= 16; i++) {
+			fishs.add(new Integer[] { 0, 0, 0, 0 });
+		}
+		for (int i = 1; i < n + 1; i++) {
+			for (int j = 1; j < n + 1; j++) {
+				map[i][j] = sc.nextInt();
+				Integer[] fish = fishs.get(map[i][j]);
+				fish[0] = i;
+				fish[1] = j;
+				fish[2] = sc.nextInt() - 1;
 			}
-			sb.append(Math.max(dp[1][n+1], dp[0][n+1])).append("\n");
-		} // end of testCases
-		System.out.println(sb);
-	} // end of main
-} // end of class
+		}
+		start(1, 1, 0, map, fishs);
+		System.out.println(max_cnt);
+		sc.close();
+	}
+
+	private static void start(int y, int x, int cnt, int[][] map, List<Integer[]> fishs) {
+		cnt += map[y][x]; // 현재 위치의 물고기 먹음
+		max_cnt = Math.max(cnt, max_cnt); // 카운트 갱신
+		int sharkDir = fishs.get(map[y][x])[2]; // 먹은 물고기의 방향 가짐
+		fishs.get(map[y][x])[3] = died; // 먹은 물고기의 상태를 쥬금으로 바꿈
+		map[y][x] = shark; // 먹은 물고기 위치를 상어로 바꿈
+		for (int i = 1; i <= 16; i++) { // 1번 부터 16번 물고기 이동
+			Integer[] fish = fishs.get(i);
+			int fish_y = fish[0];
+			int fish_x = fish[1];
+			int fish_dir = fish[2];
+			int fish_status = fish[3];
+			if (fish_status != died) { //죽은 물고기가 아니라면
+				for (int j = fish_dir; j < fish_dir + 8; j++) { // 현재 물고기 방향에서 45씩 8회전
+					int ny = fish_y + dy[j % 8];
+					int nx = fish_x + dx[j % 8];
+					if (map[ny][nx] != shark) { // 상어 없으면 이동
+						fish[0] = ny; // 물고기의 위치와 방향 재설정
+						fish[1] = nx;
+						fish[2] = j % 8;
+						if (map[ny][nx] != died) { // 죽은 애가 아니면
+							Integer[] swap_fish = fishs.get(map[ny][nx]); // 있던 물고기 데리고옴
+							swap_fish[0] = fish_y; // 있던 물고기의 좌표 변경해서 저장
+							swap_fish[1] = fish_x;
+						}
+						int temp = map[ny][nx]; // map의 물고기 위치 변경
+						map[ny][nx] = map[fish_y][fish_x];
+						map[fish_y][fish_x] = temp;
+						break;
+					}
+				}
+			}
+		}
+		
+		map[y][x] = died; // map의 상어가 떠난 자리는 쥬금으로 변경
+		int sy = y + dy[sharkDir];
+		int sx = x + dx[sharkDir];
+		while (sy < n + 1 && sy > 0 && sx < n + 1 && sx > 0) {
+			if (map[sy][sx] != died)
+				start(sy, sx, cnt, copy_map(map), copy_fishs(fishs));
+			sy += dy[sharkDir];
+			sx += dx[sharkDir];
+		}
+		return;
+	}
+
+	private static List<Integer[]> copy_fishs(List<Integer[]> fishs) { // addAll로 카피할랬다가 2시간 날림
+		List<Integer[]> list = new ArrayList<Integer[]>();
+		for(int i = 0; i < fishs.size(); i++) {
+			list.add(new Integer[] {fishs.get(i)[0], 
+					fishs.get(i)[1], fishs.get(i)[2], fishs.get(i)[3]});
+		}
+		return list;
+	}
+
+	private static int[][] copy_map(int[][] map) {
+		int[][] cp_map = new int[n + 2][n + 2];
+		for (int i = 0; i < n + 2; i++) {
+			for (int j = 0; j < n + 2; j++) {
+				cp_map[i][j] = map[i][j];
+			}
+		}
+		return cp_map;
+	}
+}
